c80667cd148779c2ffa5720ddc683cd53eecc834
hector <hectorcoldwell@gmail.com>
2024-11-22 15:07:13 +0000

    Commented out Duration.java, Readers working as intended

--- src/Duration.java
+++ src/Duration.java
@@ -1,22 +1,22 @@
-public class Duration implements Comparable<Duration> {
-    private int hours;
-    private int minutes;
-    private int seconds;
-    public Duration(int hours, int minutes, int seconds) {
-        this.hours = hours;
-        this.minutes = minutes;
-        this.seconds = seconds;
-    }
-    public int toSeconds(){
-        return(hours * 3600 + minutes * 60 + seconds);
-    }
-    public int compareTo(Duration other) {
-        return (Integer.compare(this.toSeconds(), other.toSeconds()));
-    }
-
-    public String toString() {
-
-    }
-
-
-}
+//public class Duration implements Comparable<Duration> {
+//    private int hours;
+//    private int minutes;
+//    private int seconds;
+//    public Duration(int hours, int minutes, int seconds) {
+//        this.hours = hours;
+//        this.minutes = minutes;
+//        this.seconds = seconds;
+//    }
+//    public int toSeconds(){
+//        return(hours * 3600 + minutes * 60 + seconds);
+//    }
+//    public int compareTo(Duration other) {
+//        return (Integer.compare(this.toSeconds(), other.toSeconds()));
+//    }
+//
+//    public String toString() {
+//
+//    }
+//
+//
+//}
